42099f069293c7707f273ff562336a32ba091c0d
Kendy JEAN-MARIE <kjeanmarie@prosodie.com>
2018-02-07 21:13:01 +0100

    JHipster
    CMD line, Générateur d'application de divers types A tester de toute
    urgence! :
    
    Monolithic application (recommended for simple projects)
    Microservice application
    Microservice gateway
    JHipster UAA server (for microservice OAuth2 authentication)

---
+++ 9_jhipster_app/src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/fr';
+
+import {
+    MonolithicAppSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        MonolithicAppSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        JhiLanguageHelper,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'fr'
+        },
+    ],
+    exports: [
+        MonolithicAppSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class MonolithicAppSharedCommonModule {
+    constructor() {
+        registerLocaleData(locale);
+    }
+}
